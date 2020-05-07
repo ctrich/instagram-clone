1ec5092468563ae19951436f813a5521cb251929
Chris Richardson <ctrich86@yahoo.com>
2020-05-07 09:38:36 -0700

    added nodejs posts API

--- server/app.js
+++ server/app.js
@@ -4,9 +4,7 @@ const mongoose = require('mongoose')
 const PORT = 5000
 const {MONGOURI} = require('./keys')
 
-require('./models/user')
 
-app.use(require('./routes/auth'))
 
 mongoose.connect(MONGOURI, {
     useNewUrlParser: true,
@@ -20,6 +18,13 @@ mongoose.connection.on('connected', () => {
 mongoose.connection.on('error', (err) => {
     console.log('error connecting',err)
 })
+
+require('./models/user')
+require('./models/posts')
+
+app.use(express.json())
+app.use(require('./routes/auth'))
+app.use(require('./routes/posts'))
 
 app.listen(PORT, () => {
     console.log('The server is running on port', PORT);
